ac5bc6f65743a1b758e52f49e745aca4be946128
Andrea Ramirez <anlramirezs11@gmail.com>
2020-05-16 16:30:21 -0500

    Se crea los componentes para Header, Icon y Table

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template lang="pug">
-  main(id="app" class="bg-blue-400")
-    HelloWorld(msg="Welcome to Your Vue.js App")
+  main
+    px-Header
+    px-assets-table
 </template>
 
 <script>
 // Se importa el componente Hello Word
-import HelloWorld from "./components/HelloWorld.vue";
+import PxHeader from "@/components/PxHeader.vue"
+import PxAssetsTable from "@/components/PxAssetsTable.vue"
 
 export default {
   // Establece un nombre para el componente
   name: "App",
   // Se declaran los componentes que se van a usar.
   components: {
-    HelloWorld
+    PxHeader,
+    PxAssetsTable
   }
-};
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/PxAssetsTable.vue
@@ -0,0 +1,66 @@
+<template lang="pug">
+  table
+    thead
+        tr(class="bg-gray-100 border-b-2 border-gray-400")
+            th
+            th
+                span Ranking
+            th Nombre
+            th Precio
+            th Cap. de Mercado
+            th Variación 24hs
+            td(class="hidden sm:block")
+    tbody
+        tr(class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100")
+            td
+            td
+            td
+            td
+            td
+            td
+            td(class="hidden sm:block")
+</template>
+<script>
+export default {
+  name: "PxAssetsTable",
+  props: {
+    assetes: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style scoped>
+.up::before {
+  content: "👆";
+}
+
+.down::before {
+  content: "👇";
+}
+
+td {
+  padding: 20px 0px;
+  font-size: 0.6rem;
+  text-align: center;
+}
+
+th {
+  padding: 5px;
+  font-size: 0.6rem;
+}
+
+@media (min-width: 640px) {
+  td,
+  th {
+    padding: 20px;
+    font-size: 1rem;
+  }
+
+  th {
+    padding: 12px;
+  }
+}
+</style>
