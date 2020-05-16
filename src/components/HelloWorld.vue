01ba331917792528b40924c08e4910ec6ef6a5c1
Andrea Ramirez <anlramirezs11@gmail.com>
2020-05-16 10:15:06 -0500

    Se integra pug.js

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,8 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-  </div>
+<template lang="pug">
+  div(class="hello")
+    h1 {{ msg }}
+    p For a guide and recipes on how to configure
+      a(href="https://cli.vuejs.org" target="_blank" rel="noopener") vue-cli documentation
 </template>
 
 <script>
@@ -17,7 +11,7 @@ export default {
   props: {
     msg: String
   }
-};
+}
 </script>
 
 <!-- scoped se usa cuando los estilos solo se quieren aplicar para solo al componente -->
